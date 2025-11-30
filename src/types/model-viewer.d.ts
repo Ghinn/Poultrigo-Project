7693b7e65efc3baa58b076bf45bf566f809d3863
Muhammad Rizki <mrizki160805@gmail.com>
2025-11-30 23:27:44 +0700

    update hosting

--- src/types/model-viewer.d.ts
+++ src/types/model-viewer.d.ts
@@ -1,23 +1,25 @@
 import type React from "react";
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "model-viewer": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      src?: string;
-      poster?: string;
-      ar?: boolean;
-      "ar-modes"?: string;
-      "camera-controls"?: boolean;
-      "auto-rotate"?: boolean;
-      autoplay?: boolean;
-      exposure?: string | number;
-      "shadow-intensity"?: string | number;
-      "interaction-prompt"?: string;
-      "environment-image"?: string;
-    };
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "model-viewer": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        src?: string;
+        poster?: string;
+        ar?: boolean;
+        "ar-modes"?: string;
+        "camera-controls"?: boolean;
+        "auto-rotate"?: boolean;
+        autoplay?: boolean;
+        exposure?: string | number;
+        "shadow-intensity"?: string | number;
+        "interaction-prompt"?: string;
+        "environment-image"?: string;
+      };
+    }
   }
 }
 
